51ff16e271b6e0feb92e701e7a9bd69a58501da5
olakehinde <olakehinde59@gmail.com>
2020-05-01 01:28:41 -0700

    added php file for day1 challenge

---
+++ day1.php
@@ -0,0 +1,7 @@
+<?php
+//  write a function that takes in an integer as a parameter and returns the number of possible divisors of the number including 1 but excluding itself.
+ function divisor($x) {
+    
+    return $count;
+ }
+?>
