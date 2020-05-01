da0200740e17922d4a5a4f9c0bc23f40c2432bbb
olakehinde <olakehinde59@gmail.com>
2020-05-01 02:26:54 -0700

    added solution to Day 1 - Backend beginner's challenge

--- day1.php
+++ day1.php
@@ -1,7 +1,14 @@
 <?php
 //  write a function that takes in an integer as a parameter and returns the number of possible divisors of the number including 1 but excluding itself.
- function divisor($x) {
-    
+function divisor($x) {
+    $count = 0;
+    for ($i=1; $i < $x; $i++) { 
+        if (($x % $i) == 0) {
+            $count++;
+        }
+    }
     return $count;
- }
+}
+
+echo divisor(10);
 ?>
